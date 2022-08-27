1b3874fdd93a2eb64bc1a2bf8818a44ddea1f55a
Jack P <jack.e.perry@outlook.com>
2022-08-27 22:10:52 +0000

    cents_to_dollar macro added

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
+    {{ cents_to_dollars('amount') }} as amount,
     created as created_date,
     _batched_at
 from {{ source('stripe', 'payment') }}
